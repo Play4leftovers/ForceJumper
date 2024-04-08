da6c00ecda568d35c3ebc5b99aa7127f7ad2db49
Tor Nordmark <30878484+Play4leftovers@users.noreply.github.com>
2024-04-08 14:21:02 +0200

    Added dashing

--- Assets/Scripts/Movement/PlayerMovementPhysics.cs
+++ Assets/Scripts/Movement/PlayerMovementPhysics.cs
@@ -9,16 +9,13 @@ public class PlayerMovementPhysics : MonoBehaviour
     private Rigidbody _rb;
     private Vector2 _inputDir;
     private Vector3 _moveDir;
+
+    private bool _inputStopper = false;
     
     [Header("Movement")]
-    private float _maxSpeed;
-    private float _accelerationSpeed;
-    [SerializeField] private float sprintMaxSpeed = 12;
-    [SerializeField] private float walkMaxSpeed = 8;
-    [SerializeField] private float sprintAccelerationSpeed = 16;
-    [SerializeField] private float walkAccelerationSpeed = 10;
+    [SerializeField] private float maxSpeed = 8;
+    [SerializeField] private float accelerationSpeed = 10;
     private Vector3 _playerVelocity;
-    private bool _sprinting = false;
     
     [Header("Air and Ground Control")]
     [SerializeField] private float groundDrag;
@@ -40,16 +37,22 @@ public class PlayerMovementPhysics : MonoBehaviour
 
     [Header("Slope Control")] [SerializeField]
     private float maxSlopeAngle = 45.0f;
-    private RaycastHit slopeHit;
-    private bool exitingSlope;
+    private RaycastHit _slopeHit;
+    private bool _exitingSlope;
 
+    [Header("Dashing")] 
+    private bool _dashing;
+    [SerializeField] private float dashingDuration = 0.1f;
+    [SerializeField] private float dashingSpeed = 2.5f; //Multiplier of maxSpeed
+    [SerializeField] private float dashingAcceleration = 10.0f; //Multiplier of accelerationSpeed
 
     public MovementState state;
     public enum MovementState
     {
         Walking,
-        Sprinting,
-        Air
+        Air,
+        Dash,
+        AirDash
     }
     
     void Start()
@@ -61,6 +64,7 @@ public class PlayerMovementPhysics : MonoBehaviour
     #region Input Management
     public void Jump(InputAction.CallbackContext context)
     {
+        if (_inputStopper) return;
         if (context.performed)
         {
             _jumping = true;
@@ -78,19 +82,20 @@ public class PlayerMovementPhysics : MonoBehaviour
 
     public void Movement(InputAction.CallbackContext context)
     {
+        if (_inputStopper) return;
         _inputDir = context.ReadValue<Vector2>();
     }
 
-    public void Sprint(InputAction.CallbackContext context)
+    public void Dash(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.performed && !_dashing)
         {
-            _sprinting = true;
-        }
-
-        if (context.canceled)
-        {
-            _sprinting = false;
+            _dashing = true;
+            _inputStopper = true;
+            _exitingSlope = true;
+            maxSpeed *= dashingSpeed;
+            accelerationSpeed *= dashingAcceleration;
+            Invoke(nameof(StopDash), dashingDuration);
         }
     }
     #endregion
@@ -108,29 +113,30 @@ public class PlayerMovementPhysics : MonoBehaviour
     {
         MovePlayer();
     }
+    #endregion
 
+    #region StateHandler
     private void StateHandler()
     {
-        if (_isGrounded && _sprinting)
-        {
-            state = MovementState.Sprinting;
-            _accelerationSpeed = sprintAccelerationSpeed;
-            _maxSpeed = sprintMaxSpeed;
-        }
-        else if (!_isGrounded)
+        if (!_isGrounded && !_dashing)
         {
             state = MovementState.Air;
+        }
+        else if (!_isGrounded && _dashing)
+        {
+            state = MovementState.AirDash;
+        }
+        else if(_isGrounded && _dashing)
+        {
+            state = MovementState.Dash;
         }
         else
         {
             state = MovementState.Walking;
-            _accelerationSpeed = walkAccelerationSpeed;
-            _maxSpeed = walkMaxSpeed;
         }
     }
     #endregion
 
-    
     void GroundCheck()
     {
         _isGrounded = Physics.Raycast(transform.position, Vector3.down, _playerHeight*0.5f+0.3f, groundMask);
@@ -151,17 +157,17 @@ public class PlayerMovementPhysics : MonoBehaviour
         bool onSlope = OnSlope();
         _moveDir = transform1.right * _inputDir.x + transform1.forward * _inputDir.y;
         
-        if (onSlope && !exitingSlope)
+        if (onSlope && !_exitingSlope)
         {
-            _rb.AddForce(GetSlopeMoveDirection() * (_accelerationSpeed * 10), ForceMode.Force);
+            _rb.AddForce(GetSlopeMoveDirection() * (accelerationSpeed * 10), ForceMode.Force);
             if (_rb.velocity.y > 0)
             {
                 _rb.AddForce(Vector3.down * 80f, ForceMode.Force);
             } 
         }
         
-        if (_isGrounded) _rb.AddForce(_moveDir * (_accelerationSpeed * 10), ForceMode.Force);
-        else if(!_isGrounded) _rb.AddForce(_moveDir * (_accelerationSpeed * airSpeedMultiplier * 10), ForceMode.Force);
+        if (_isGrounded) _rb.AddForce(_moveDir * (accelerationSpeed * 10), ForceMode.Force);
+        else if(!_isGrounded) _rb.AddForce(_moveDir * (accelerationSpeed * airSpeedMultiplier * 10), ForceMode.Force);
 
         _rb.useGravity = !onSlope;
     }
@@ -170,32 +176,40 @@ public class PlayerMovementPhysics : MonoBehaviour
     {
         var tempVelocity = _rb.velocity;
 
-        if (OnSlope() && !exitingSlope)
+        if (OnSlope() && !_exitingSlope)
         {
-            if (tempVelocity.magnitude > _maxSpeed)
-                _rb.velocity = tempVelocity.normalized * _maxSpeed;
+            if (tempVelocity.magnitude > maxSpeed)
+                _rb.velocity = tempVelocity.normalized * maxSpeed;
         }
         else
         {
             Vector3 flatVelocity = new Vector3(tempVelocity.x, 0f, tempVelocity.z);
-            if (flatVelocity.magnitude > _maxSpeed)
+            if (flatVelocity.magnitude > maxSpeed)
             {
-                Vector3 limitedVelocity = flatVelocity.normalized * _maxSpeed;
+                Vector3 limitedVelocity = flatVelocity.normalized * maxSpeed;
                 _rb.velocity = new Vector3(limitedVelocity.x, _rb.velocity.y, limitedVelocity.z);
+            }
+        }
+
+        if (_dashing)
+        {
+            if (tempVelocity.y > maxSpeed / dashingSpeed)
+            {
+                _rb.velocity = new Vector3(_rb.velocity.x, maxSpeed / dashingSpeed, _rb.velocity.z);
             }
         }
     }
     
     private Vector3 GetSlopeMoveDirection()
     {
-        return Vector3.ProjectOnPlane(_moveDir, slopeHit.normal).normalized;
+        return Vector3.ProjectOnPlane(_moveDir, _slopeHit.normal).normalized;
     }
     
     bool OnSlope()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, _playerHeight * 0.5f + 0.3f))
+        if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, _playerHeight * 0.5f + 0.3f))
         {
-            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
             if (angle < maxSlopeAngle && angle != 0) return true;
             else return false;
         }
@@ -214,7 +228,7 @@ public class PlayerMovementPhysics : MonoBehaviour
     
     void Jumping()
     {
-        exitingSlope = true;
+        _exitingSlope = true;
         _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
         _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
@@ -229,6 +243,15 @@ public class PlayerMovementPhysics : MonoBehaviour
     void ResetJump()
     {
         _readyToJump = true;
-        exitingSlope = false;
+        _exitingSlope = false;
+    }
+
+    void StopDash()
+    {
+        maxSpeed /= dashingSpeed;
+        accelerationSpeed /= dashingAcceleration;
+        _dashing = false;
+        _inputStopper = false;
+        _exitingSlope = true;
     }
 }
