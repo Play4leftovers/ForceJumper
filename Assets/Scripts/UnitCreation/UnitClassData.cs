a694d5ed7d5a3c3a89d54d1416bfea861cdf2927
Xaxroth <Xaxroth@gmail.com>
2024-04-11 02:59:26 +0200

    Turret base

---
+++ Assets/Scripts/UnitCreation/UnitClassData.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitClassData", menuName = "Custom/New Unit Class", order = 1)]
+public class UnitClassData : ScriptableObject
+{
+    public string className;
+    public string classDescription;
+
+    public bool promotedClass;
+
+    [Header("Base Stats")]
+
+    public int defaultMovementRange;
+    public int defaultHealth;
+    public int defaultPower;
+    public int defaultIntelligence;
+    public int defaultSpeed;
+    public int defaultAim;
+    public int defaultReflexes;
+    public int defaultAttackSpeed;
+
+    [Header("Growth Rates")]
+
+    [Range(0, 1)] public float healthGrowth;
+    [Range(0, 1)] public float powerGrowth;
+    [Range(0, 1)] public float intelligenceGrowth;
+    [Range(0, 1)] public float speedGrowth;
+    [Range(0, 1)] public float aimGrowth;
+    [Range(0, 1)] public float reflexesGrowth;
+    [Range(0, 1)] public float attackSpeedGrowth;
+
+    public enum UnitType
+    {
+        Melee,
+        Ranged
+    }
+
+    public UnitType UnitMobilityType;
+}
